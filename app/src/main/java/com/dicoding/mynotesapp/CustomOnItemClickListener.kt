bcd09cda201114f15fa4112b72bc283f3e85592d
idewaputuwiprah <37007528+idewaputuwiprah@users.noreply.github.com>
2020-12-15 14:03:18 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/mynotesapp/CustomOnItemClickListener.kt
@@ -0,0 +1,14 @@
+package com.dicoding.mynotesapp
+
+import android.view.View
+
+class CustomOnItemClickListener(private val position: Int, private val onItemClickCallback: OnItemClickCallback): View.OnClickListener {
+
+    interface OnItemClickCallback {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    override fun onClick(view: View) {
+        onItemClickCallback.onItemClick(view, position)
+    }
+}
